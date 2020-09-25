56bcece488a4ac2dc5da73daaac6a4e99ec8dfeb
Ana Cabanach <stefffy2014@gmail.com>
2020-09-25 16:37:26 +0200

    cambios 3 en raya

--- UD01-Entorno trabajo/code.js
+++ UD01-Entorno trabajo/code.js
@@ -1,6 +1,6 @@
 var tablero = new Array;
 tablero[0] = ["_","_","_"];
-tablero[1] = ["_","_","_"];
+tablero[1] = ["_","o","_"];
 tablero[2] = ["_","_","_"];
 
 console.log(tablero);
@@ -12,6 +12,7 @@ function pinto_tablero(){
         for(j=0;j<=2;j++){
             var id_casilla = "casilla_" + i + j;
             document.getElementById(id_casilla).value=tablero[i][j];
+            //AQUI FALTA UN IF
         }
     
     }
@@ -25,11 +26,26 @@ function pinto_tablero(){
 }
 pinta un barra baja*/
 
-function jugada(){
+function leoTablero(){
  //leer todos los valores de los inputs y meterlos en tablero
 
+for(i=0;i<tablero.length;i++){
+    for(j=0;tablero.length;i++){
+        var id_casilla = "casilla_" + i + j;
+        tablero[i][j] = document.getElementById(id_casilla).value;
 
 
+        if((tablero[i][j] != "O") && (tablero != "_")){
+            tablero[i][j] ="X";
+        } 
+    }
+    
+}
+
+console.log(tablero);
+compruebaGanador();
+tiradaMaquia();
+compruebaGanador();
   //  var valor_01 = document.getElementById("casilla_01").value;
   //  console.log(valor_01);
   //  recoje valor de solo el 01
@@ -37,11 +53,52 @@ function jugada(){
 
 function tiradaMaquia(){
     //aleatorio para añadir 0
-    var aleatorio_o = Math.floor(Math.random() * 2);
-    document.getElementById("casilla_aleatoria").value = tablero[aleatorio_o][aleatorio_o];
-    tablero.replace("_", "o");
+
+    jugada_x = Math.floor(Math.random() * 3);
+    jugada_y = Math.floor(Math.random() * 3);
+
+    console.log("jugadax" + jugada_x + "jugaday" + jugada_y) ;
+    if(tablero[jugada_x][jugada_y] == "_"){
+        tablero[jugada_x][jugada_y] = "o";
+        pinto_tablero();
+
+    }else{
+        
+        tiradaMaquia();
+       
+    }
+
+
+    /*document.getElementById("casilla_aleatoria").value = tablero[aleatorio_o][aleatorio_o];
+    tablero.replace("_", "o");*/
 }
 
 function comprobarJugador(){
     //comprobar si fila 1 2 y 3 son iguals para completar la diagonal
+
+    if((tablero[0][0] == tablero[1][0]) && (tablero[1][0] == tablero[2][0])  && (tablero[0][0] != "_")){
+        alert("Has ganado");
+        location.reload();
+    }
+
+    if((tablero[0][1] == tablero[1][1]) && (tablero[1][1] == tablero[2][1])  && (tablero[0][1] != "_")){
+        alert("Has ganado");
+        location.reload();
+    }
+
+    if((tablero[0][2] == tablero[1][2]) && (tablero[1][2] == tablero[2][2])  && (tablero[0][2] != "_")){
+        alert("Has ganado");
+        location.reload();
+    }
+
+    if((tablero[0][0] == tablero[1][1]) && (tablero[1][1] == tablero[2][2])  && (tablero[0][0] != "_")){
+        alert("Has ganado");
+        location.reload();
+    }
+
+    if((tablero[0][2] == tablero[1][1]) && (tablero[1][1] == tablero[2][0])  && (tablero[0][2] != "_")){
+        alert("Has ganado");
+        location.reload();
+    }
+    
 }
